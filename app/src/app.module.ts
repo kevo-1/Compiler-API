de5c3851168f09787ffb4888f3a3e77ef38d142f
kevo-1 <kevin.bastawrous@gmail.com>
2025-11-25 04:30:23 +0200

    feat(cEnv): Implemented C route

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -5,6 +5,7 @@ import { JSCompilerService } from './sandboxEnv/jsEnv/js.service';
 import { TSCompilerService } from './sandboxEnv/tsEnv/ts.service';
 import { PyCompilerService } from './sandboxEnv/pyEnv/py.service';
 import { GoCompilerService } from './sandboxEnv/goEnv/go.service';
+import { CCompilerService } from './sandboxEnv/cEnv/c.service';
 import { CompilationQueueService } from './compilation-queue.service';
 import { DockerCleanupService } from './docker-cleanup.service';
 
@@ -17,6 +18,7 @@ import { DockerCleanupService } from './docker-cleanup.service';
         TSCompilerService,
         PyCompilerService,
         GoCompilerService,
+        CCompilerService,
         CompilationQueueService,
         DockerCleanupService,
     ],
